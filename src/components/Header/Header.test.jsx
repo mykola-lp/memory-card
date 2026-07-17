d04988bca3ed4df7ac3f2b1f39a4291e02eeaa1e
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-17 11:15:06 +0300

    test snapshot for header component

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import Header from './Header';
+
+test('matches snapshot', () => {
+  const { container } = render(<Header />);
+  expect(container).toMatchSnapshot();
+});
