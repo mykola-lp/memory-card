f89a81ba2b706ae38ed0f52cd9fd50139a0c5453
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-17 10:55:56 +0300

    vite tests config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/memory-card/' : '/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
 }));
