83cea8054d64c69b083a48fddd91cdc6429151fd
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-17 11:05:42 +0300

    test event for card component

---
+++ src/components/Card/Card.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Card from './Card';
+
+test('calls onClick with pokemon id when clicked', async () => {
+  const handleClick = vi.fn();
+  const pokemon = { id: 1, name: 'pikachu', image: 'pikachu.png' };
+
+  render(<Card pokemon={pokemon} onClick={handleClick} />);
+
+  const card = screen.getByText(/pikachu/i);
+  await userEvent.click(card);
+
+  expect(handleClick).toHaveBeenCalledWith(1);
+});
